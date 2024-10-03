91b36ec4692c0ad602ade5f6d7fde9b56edecb15
ywaf <52742690+ywaf@users.noreply.github.com>
2024-10-03 16:41:57 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+export default {
+	async fetch(request, env, ctx) {
+		let value = await env.count.get("total");
+		let valueint = parseInt(value);
+		console.log(valueint)
+		const httpresponse = await fetch("https://api.humanitix.com/v1/events/" + env.humanitixeventid + "/tickets?page=1&pageSize=1", {
+			headers: {
+				"x-api-key": env.humanitixapikey,
+			},
+			});
+		const jsonresp = await httpresponse.json()
+		const totalsold = parseInt(jsonresp.total, 10)
+		// console.log(jsonresp)
+
+		if (totalsold > valueint) {
+			console.log("change!")
+			await env.count.put("total", totalsold.toString())
+			const myHeaders = new Headers();
+			myHeaders.append("Content-Type", "text/plain");
+
+			const raw = '{"totalsalesnumber":"' + totalsold.toString() + '", "totalsalesdiff": "' + (totalsold - valueint).toString() + '", "totalsalesprevious": "' + valueint.toString() + '"}';
+
+			const requestOptions = {
+				method: "POST",
+				headers: myHeaders,
+				body: raw,
+				redirect: "follow"
+			};
+
+			const resptwo = await fetch(env.slackwebhook, requestOptions)
+			console.log(resptwo.text())
+		} else {
+			console.log("no change needed")
+		}
+
+		return new Response(totalsold);
+	},
+};
